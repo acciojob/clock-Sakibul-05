968b2e39f279ee43c483056ba365ed9d5703b160
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-06-20 10:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function updateTimer() {
+      var timerElement = document.getElementById("timer");
+      var currentDate = new Date();
+      timerElement.textContent = currentDate.toLocaleString();
+    }
+
+    setInterval(updateTimer, 1000); // Update the timer every second
